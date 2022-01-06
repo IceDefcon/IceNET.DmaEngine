2e1c279d9653d086e96c72adcb1313c55589263b
IceDefcon <ice.defcon@yahoo.com>
2022-01-06 23:30:51 +0100

    version 1.1.0

--- link.asm
+++ link.asm
@@ -1,5 +1,90 @@
-global input
-input:
-	add rdi,2000
-	mov rax,rdi
+global _RAX
+global _RBX
+global _RCX
+global _RDX
+global _RSP
+global _RBP
+global _RSI
+global _RDI
+
+global _RSS
+global _RCS
+global _RDS
+global _RES
+global _RFS
+global _RGS
+
+global _EFLAGS
+
+_RAX:
+	ret
+
+_RBX:
+	mov rax, rbx
+	ret
+
+_RCX:
+	mov rax, rcx
+	ret
+
+_RDX:
+	mov rax, rdx
+	ret
+
+_RSP:
+	mov rax, rsp
+	ret
+
+_RBP:
+	mov rax, rbp
+	ret
+
+_RSI:
+	mov rax, rsi
+	ret
+
+_RDI:
+	mov rax, rdi
+	ret
+
+
+_RSS:
+	mov rax, ss
+	ret
+
+_RCS:
+	mov rax, cs
+	ret
+
+_RDS:
+	mov rax, ds
+	ret
+
+_RES:
+	mov rax, es
+	ret
+
+_RFS:
+	mov rax, fs
+	ret
+
+_RGS:
+	mov rax, gs
+	ret
+
+_EFLAGS:
+	pushf  			; push eflags into stack
+	pop rax 		; pop it into rax
+	ret
+
+global Return_4x_16bit_arguments
+
+Return_4x_16bit_arguments:
+	mov rax, rdi
+	shl rax, $10
+	add rax, rsi
+	shl rax, $10
+	add rax, rdx
+	shl rax, $10
+	add rax, rcx
 	ret
--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <iostream>
+#include <unistd.h>
+
+#include "Dump.h"
+
 using namespace std;
 
 #define ADDRESS_SPACE 8
@@ -9,18 +13,14 @@ struct dma_engine
     int *Address[ADDRESS_SPACE] = {nullptr};
 };
 
-//////////////////////////////////////////////////////
-// Hacking DMA address trough the CPU register pairs
-//////////////////////////////////////////////////////
-extern "C" int input(int param);
-int output(void) { return input(6); }
-//////////////////////////////////////////////////////
+extern "C" unsigned long Return_4x_16bit_arguments(int arg_1, int arg_2, int arg_3, int arg_4);
 
 int main(int argc, char* argv[]) {
 
+    system("clear");
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////
     // Testing definition of null pointers in the struct
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////a////////////////////////////////////////////////////
     dma_engine Dma_0;
 
     for(int i = 0;i <ADDRESS_SPACE;i++)
@@ -39,8 +39,20 @@ int main(int argc, char* argv[]) {
     printf("\n");
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    int result = output();
-    printf("Addition in assembly ---> expected: 2000 + 6 = 0x7d6 \n");
-    printf("The result is: ---> %x\n",result);
+    Return_4x_16bit_arguments(2,4,6,8);
+    Register_Dump();
+    long long argumetns = Return_4x_16bit_arguments(2,4,6,8);
+
+    printf("argumetns = %llx \n",argumetns);
+
+    int argumetn_1 = (0xffff000000000000 & argumetns) >> 0x30;
+    int argumetn_2 = (0x0000ffff00000000 & argumetns) >> 0x20;
+    int argumetn_3 = (0x00000000ffff0000 & argumetns) >> 0x10;
+    int argumetn_4 = (0x000000000000ffff & argumetns) >> 0x00;
+
+    printf("argumetn_1 = %x \n",argumetn_1);
+    printf("argumetn_2 = %x \n",argumetn_2);
+    printf("argumetn_3 = %x \n",argumetn_3);
+    printf("argumetn_4 = %x \n",argumetn_4);
     return 0;
 }
