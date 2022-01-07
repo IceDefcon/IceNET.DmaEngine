2d6b40ba0a9ec9d15112d9610d4389e3d927af86
IceDefcon <ice.defcon@yahoo.com>
2022-01-07 03:29:43 +0100

    version 1.2.1

--- link.asm
+++ link.asm
@@ -1,84 +1,10 @@
-global _RAX
-global _RBX
-global _RCX
-global _RDX
-global _RSP
-global _RBP
-global _RSI
-global _RDI
-
-global _RSS
-global _RCS
-global _RDS
-global _RES
-global _RFS
-global _RGS
-
 global _EFLAGS
-
-_RAX:
-	ret
-
-_RBX:
-	mov rax, rbx
-	ret
-
-_RCX:
-	mov rax, rcx
-	ret
-
-_RDX:
-	mov rax, rdx
-	ret
-
-_RSP:
-	mov rax, rsp
-	ret
-
-_RBP:
-	mov rax, rbp
-	ret
-
-_RSI:
-	mov rax, rsi
-	ret
-
-_RDI:
-	mov rax, rdi
-	ret
-
-
-_RSS:
-	mov rax, ss
-	ret
-
-_RCS:
-	mov rax, cs
-	ret
-
-_RDS:
-	mov rax, ds
-	ret
-
-_RES:
-	mov rax, es
-	ret
-
-_RFS:
-	mov rax, fs
-	ret
-
-_RGS:
-	mov rax, gs
-	ret
-
 _EFLAGS:
 	pushf  			; push eflags into stack
 	pop rax 		; pop it into rax
 	ret
 
 global Return_4x_16bit_arguments
-
 Return_4x_16bit_arguments:
 	mov rax, rdi
 	shl rax, $10
@@ -88,3 +14,74 @@ Return_4x_16bit_arguments:
 	shl rax, $10
 	add rax, rcx
 	ret
+	
+global _RAX
+_RAX:
+	ret
+
+global _RBX
+_RBX:
+	mov rax, rbx
+	ret
+
+global _RCX
+_RCX:
+	mov rax, rcx
+	ret
+
+global _RDX
+_RDX:
+	mov rax, rdx
+	ret
+
+global _RSP
+_RSP:
+	mov rax, rsp
+	ret
+
+global _RBP
+_RBP:
+	mov rax, rbp
+	ret
+
+global _RSI
+_RSI:
+	mov rax, rsi
+	ret
+
+global _RDI
+_RDI:
+	mov rax, rdi
+	ret
+
+
+global _RSS
+_RSS:
+	mov rax, ss
+	ret
+
+global _RCS
+_RCS:
+	mov rax, cs
+	ret
+
+global _RDS
+_RDS:
+	mov rax, ds
+	ret
+
+global _RES
+_RES:
+	mov rax, es
+	ret
+
+global _RFS
+_RFS:
+	mov rax, fs
+	ret
+
+global _RGS
+_RGS:
+	mov rax, gs
+	ret
+
