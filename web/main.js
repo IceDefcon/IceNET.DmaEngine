78e7da9c71e17ff26ce4dc3f9c4866b152858fe8
IceDefcon <ice.defcon@yahoo.com>
2022-02-10 14:33:56 +0100

    Version 5.1.0 Add Website Code

---
+++ web/main.js
@@ -0,0 +1,42 @@
+// code to get the JSON data (function to load the data)
+//    pseudo    GET data.json
+//   var myData = result of GET data.json
+
+var myData1 = "Current";
+var myData2 = "Voltage";
+var myData3 = "Real Power";
+var myData4 = "Apparent Power";
+var myData5 = "Reactive Power";
+var myData6 = "Power factor";
+var myData7 = "Phase Imbalance";
+var myData8 = "Total Harmonic Distortion";
+var myData9 = "Higher Harmonics";
+
+
+		
+		function button1() {
+    	document.getElementById("data").innerHTML = myData1;}
+
+		function button2(){		
+		document.getElementById("data").innerHTML = myData2;}
+		
+		function button3(){
+		document.getElementById("data").innerHTML = myData3;}
+		
+		function button4(){
+		document.getElementById("data").innerHTML = myData4;}
+		
+		function button5(){
+		document.getElementById("data").innerHTML = myData5;}
+		
+		function button6(){
+		document.getElementById("data").innerHTML = myData6;}
+		
+		function button7(){
+		document.getElementById("data").innerHTML = myData7;}
+		
+		function button8(){
+		document.getElementById("data").innerHTML = myData8;}
+		
+		function button9(){
+		document.getElementById("data").innerHTML = myData9;}
