3f6155853dd2cc17862d48545a00778c4a3e4369
IceDefcon <ice.defcon@yahoo.com>
2022-02-10 15:00:55 +0100

    Version 5.1.2

--- web/main.js
+++ web/main.js
@@ -1,6 +1,5 @@
-// code to get the JSON data (function to load the data)
-//    pseudo    GET data.json
-//   var myData = result of GET data.json
+
+
 
 var myData1 = "Address";
 var myData2 = "Data";
