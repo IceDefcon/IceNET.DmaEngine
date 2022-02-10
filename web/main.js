24ca01363a0d57cad70f3874f35787035d72ea52
IceDefcon <ice.defcon@yahoo.com>
2022-02-10 14:48:20 +0100

    Version 5.1.1

--- web/main.js
+++ web/main.js
@@ -2,15 +2,9 @@
 //    pseudo    GET data.json
 //   var myData = result of GET data.json
 
-var myData1 = "Current";
-var myData2 = "Voltage";
-var myData3 = "Real Power";
-var myData4 = "Apparent Power";
-var myData5 = "Reactive Power";
-var myData6 = "Power factor";
-var myData7 = "Phase Imbalance";
-var myData8 = "Total Harmonic Distortion";
-var myData9 = "Higher Harmonics";
+var myData1 = "Address";
+var myData2 = "Data";
+
 
 
 		
@@ -19,24 +13,3 @@ var myData9 = "Higher Harmonics";
 
 		function button2(){		
 		document.getElementById("data").innerHTML = myData2;}
-		
-		function button3(){
-		document.getElementById("data").innerHTML = myData3;}
-		
-		function button4(){
-		document.getElementById("data").innerHTML = myData4;}
-		
-		function button5(){
-		document.getElementById("data").innerHTML = myData5;}
-		
-		function button6(){
-		document.getElementById("data").innerHTML = myData6;}
-		
-		function button7(){
-		document.getElementById("data").innerHTML = myData7;}
-		
-		function button8(){
-		document.getElementById("data").innerHTML = myData8;}
-		
-		function button9(){
-		document.getElementById("data").innerHTML = myData9;}
