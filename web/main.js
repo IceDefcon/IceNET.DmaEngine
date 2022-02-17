63851350c64dda3aaa2750bae5ccc48690baf68b
IceDefcon <ice.defcon@yahoo.com>
2022-02-17 02:44:36 +0100

    Version 6.4.0 Website backup

---
+++ web/main.js
@@ -0,0 +1,14 @@
+
+
+
+var myData1 = "Address";
+var myData2 = "Data";
+
+
+
+		
+		function button1() {
+    	document.getElementById("data").innerHTML = myData1;}
+
+		function button2(){		
+		document.getElementById("data").innerHTML = myData2;}
